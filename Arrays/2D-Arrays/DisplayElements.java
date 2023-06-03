d766a77bafeb9d1d2c87a2099bb9d1f9920d82ea
Domonique <dnelthr@gmail.com>
2023-06-03 07:46:13 -0400

    Deep Diivng into 2D-Arrays

---
+++ Arrays/2D-Arrays/DisplayElements.java
@@ -0,0 +1,23 @@
+public class DisplayElements
+{
+   public static void main(String[] arg)
+  {
+      // Declare a 2D array with 3 rows 
+      // and 4 columns 
+      
+      int[][] numbers = {{1, 2, 3, 4},
+                         {5, 6, 7, 8},
+                         {9, 10, 11, 12}};
+                         
+     for (int row = 0; row < 3; row++)
+     {
+         for (int col = 0; col < 4; col++)
+         {
+         
+         }
+     }
+
+   }// main end
+   
+
+}// display elements end
---
+++ Arrays/2D-Arrays/IntializingTwoDArrays.java
@@ -0,0 +1,16 @@
+/*
+   This program demonstrates how to intialize values in a 2d array
+*/
+public class IntializingTwoDArrays{
+    public static void main(String[] args)
+    {
+      /*
+         when initializing a two dimensional array, you enclose each row's inilialization list in its
+          own set of braces
+      */
+      
+      int[][] numbers = { {1,2,3},{4,5,6},{7,8,9} };
+      
+    }// main end
+
+}//IntializingTwoDArrays
